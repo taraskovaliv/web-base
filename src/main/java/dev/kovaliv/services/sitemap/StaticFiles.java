85f0cd391cd6e38fe40f71db5c1de02f58a4c2a1
Taras Kovaliv <taraskovaliv@outlook.com>
2024-09-22 00:14:58 +0300

    fix(): Rewrite image paths get.

--- src/main/java/dev/kovaliv/services/sitemap/StaticFiles.java
+++ src/main/java/dev/kovaliv/services/sitemap/StaticFiles.java
@@ -1,36 +1,37 @@
 package dev.kovaliv.services.sitemap;
 
 import cz.jiripinkas.jsitemapgenerator.robots.RobotsRule;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+@Slf4j
 public class StaticFiles {
 
-    public static Set<String> getImagePaths() throws URISyntaxException, IOException {
-        URL resource = StaticFiles.class.getResource("/static/img");
-        if (Objects.isNull(resource)) {
-            return Set.of();
+    public static Set<String> getImagePaths() {
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            URL url = loader.getResource("static/img");
+            if (url != null) {
+                File[] files = new File(url.getPath()).listFiles();
+                if (files != null) {
+                    return Arrays.stream(files)
+                            .filter(file -> !Files.isDirectory(file.toPath()))
+                            .map(File::getName)
+                            .map(fileName -> "/img/" + fileName)
+                            .collect(Collectors.toSet());
+                }
+            }
+        } catch (Exception e) {
+            log.warn("Error getting image paths", e);
         }
-        URI uri = resource.toURI();
-        try (Stream<Path> stream = Files.list(Paths.get(uri))) {
-            return stream
-                    .filter(file -> !Files.isDirectory(file))
-                    .map(Path::getFileName)
-                    .map(Path::toString)
-                    .map(fileName -> "/img/" + fileName)
-                    .collect(Collectors.toSet());
-        }
+        return Set.of();
     }
 
     public static List<RobotsRule> staticPathsRules() {
