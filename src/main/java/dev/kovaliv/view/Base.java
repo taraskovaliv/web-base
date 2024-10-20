9084488834c8f26aa16afee9c6aabae4ec0efcf7
Taras Kovaliv <taraskovaliv@outlook.com>
2024-10-20 16:10:29 +0300

    fix(): Add Multiplex ad.

--- src/main/java/dev/kovaliv/view/Base.java
+++ src/main/java/dev/kovaliv/view/Base.java
@@ -26,6 +26,8 @@ import static java.util.Objects.requireNonNullElse;
 
 public class Base {
 
+    public static final String AD_CLIENT = "ca-pub-3029870500249130";
+
     public static String getHtml(HtmlTag htmlTag) {
         return "<!DOCTYPE html>" + htmlTag.render();
     }
@@ -200,18 +202,32 @@ public class Base {
     }
 
     public static DomContent[] getHorizontalAd() {
-        String adClient = "ca-pub-3029870500249130";
         return new DomContent[]{
                 script()
-                        .attr("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + adClient)
+                        .attr("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + AD_CLIENT)
                         .attr("crossorigin", "anonymous")
                         .attr("async"),
                 ins().withClass("adsbygoogle")
                         .attr("style", "display:block")
-                        .attr("data-ad-client", adClient)
+                        .attr("data-ad-client", AD_CLIENT)
                         .attr("data-ad-slot", "7650451464")
                         .attr("data-ad-format", "auto")
                         .attr("data-full-width-responsive", "true"),
+                script().withText("(adsbygoogle = window.adsbygoogle || []).push({});")
+        };
+    }
+
+    public static DomContent[] getHorizontalMultiplexAd() {
+        return new DomContent[]{
+                script()
+                        .attr("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + AD_CLIENT)
+                        .attr("crossorigin", "anonymous")
+                        .attr("async"),
+                ins().withClass("adsbygoogle")
+                        .attr("style", "display:block")
+                        .attr("data-ad-client", AD_CLIENT)
+                        .attr("data-ad-slot", "2030760442")
+                        .attr("data-ad-format", "autorelaxed"),
                 script().withText("(adsbygoogle = window.adsbygoogle || []).push({});")
         };
     }
